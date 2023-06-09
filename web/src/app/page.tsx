27aac7b9abbe9b05e7fee19e8617cbab55fb10e2
Du2Du <eduardohilario.eha@gmail.com>
2023-06-09 11:23:17 -0300

    feat: implementando prototipo da pagina inicial

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,28 @@
-
+import Link from "next/link";
+import styles from "./Home.module.scss";
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.homeMain}>
+      <nav className={styles.homeNavBar}>
+        <span className={styles.title}>ACE System</span>
+        <div className="flex items-center gap-3">
+          <Link href={"/login"} className={"outlineButton rounded-full"}>
+            Entrar
+          </Link>
+          <Link href={"/register"} className={"fillButton rounded-full"}>
+            Cadastrar
+          </Link>
+        </div>
+      </nav>
+      <section>
+        <div className={styles.pageTitle}>
+          O sistema de análise de perfil comportamental
+        </div>
+        <Link href={"/about"} className="outlineButton rounded-full mt-3">
+          Saiba mais
+        </Link>
+      </section>
     </main>
   );
 }
