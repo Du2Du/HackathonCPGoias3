2a73abf0af7558351aaaba348f28120092ce096f
Du2Du <eduardohilario.eha@gmail.com>
2023-06-08 22:25:20 -0300

    feat: generate server project

---
+++ server/src/test/java/com/ifdevs/ace/AceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifdevs.ace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
